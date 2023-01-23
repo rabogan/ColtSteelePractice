804c7824b64590045c4ea55d83f032e1301ee297
Robert Bogan <rajbogan@outlook.com>
2023-01-23 14:40:26 -0800

    Add Simple Guessing Game

---
+++ javascriptVessels/guess.js
@@ -0,0 +1,24 @@
+let maximum = parseInt(prompt('Please enter a value between 1 to 100'));
+while (!maximum || maximum < 1 || maximum > 100) {
+    maximum = parseInt(prompt('Try again!  Please enter a valid number [1-100]:'));
+}
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+
+let guess = prompt('Enter your first guess, or use "q" to quit!');
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum)
+        guess = prompt('Too high!  Enter a new guess!');
+    else
+        guess = prompt('Too low.  Enter a new guess:');
+}
+
+if (guess === 'q') {
+    console.log('Okay, quitter!');
+} else {
+    console.log('A winner is you!');
+    console.log(`You took ${attempts} guesses to get it!`);
+}
